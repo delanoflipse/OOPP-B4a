8f2d9a1bb9a0e4f8ae2e5676210e603a5baaf1bd
Rick de Boer <rick.deboer@live.nl>
2017-11-29 15:54:37 +0100

    Added question class

---
+++ src/main/java/imgQuestions.java
@@ -0,0 +1,16 @@
+public class imgQuestion {
+
+    private String question;
+    private String path;
+
+    public imgQuestion(String question, String path){
+
+        this.question = question;
+        this.path = path;
+    }
+
+    public String toString(){
+
+
+    }
+}
