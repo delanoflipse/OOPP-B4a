b3bfc1190d1eec20ecb8e5a94ded19071e65dda5
justyanaaa <33684883+justyanaaa@users.noreply.github.com>
2017-12-06 14:55:04 +0100

    Fixed tests

--- src/test/java/KeyValuePairTest.java
+++ src/test/java/KeyValuePairTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class KeyValuePairTest {
     @Test
     void valueAsInt() {
-        KeyValuePair res = new KeyValuePair("some key", "[^\\d.]7");
+        KeyValuePair res = new KeyValuePair("some key", "asdasdasdasd7");
         assertEquals(7,res.valueAsInt());
     }
 
