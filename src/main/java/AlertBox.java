245567747dbd30a6e5fecfbc0050e53c0f1c5348
Rick de Boer <rick.deboer@live.nl>
2017-12-12 15:09:22 +0100

    imgQuestion gui + alertbox class

---
+++ src/main/java/AlertBox.java
@@ -0,0 +1,41 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class AlertBox {
+
+    public static void Display(String title, String alert){
+
+        Stage window = new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+
+        Label label = new Label();
+        label.setText(alert);
+        Button closeButton = new Button("Ok");
+        closeButton.setOnAction(e -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, closeButton);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+}
---
+++ src/main/java/imgQuestionGUI.java
@@ -0,0 +1,57 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class imgQuestionGUI extends Application{
+
+    Stage window;
+    Button nextQuestion;
+    Label question;
+    Image questionImg;
+    ImageView questionImgV;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        window = primaryStage;
+        window.setTitle("title");
+
+        nextQuestion = new Button("Next question");
+        question = new Label("Click my eyes");
+        questionImg = new Image("file:///C:/Documents/knul.jpg");
+        questionImgV = new ImageView();
+        questionImgV.setImage(questionImg);
+        questionImgV.setFitWidth(100);
+        questionImgV.setFitHeight(100);
+        questionImgV.setPreserveRatio(false);
+        questionImgV.setSmooth(true);
+        questionImgV.setCache(true);
+        questionImgV.setOnMouseClicked(e -> AlertBox.Display("Nice!", "U did it, great job!!!"));
+
+        StackPane layout = new StackPane();
+        layout.getChildren().addAll(question, questionImgV, nextQuestion);
+        Scene question1 = new Scene(layout, 300, 250);
+        window.setScene(question1);
+        window.show();
+    }
+}
